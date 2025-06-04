bdbf8d019e6475cccc20d6cbe90f5c701a86928e
Iluje <juliend1305@gmail.com>
2025-06-04 19:57:29 +0200

    A Canva look Camera, Spawn Upgrade, fixe Animation

---
+++ Assets/Julien/Script/HUD/LookCamera.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace Julien.Script.HUD
+{
+    public class LookCamera : MonoBehaviour
+    {
+        [SerializeField] private GameObject _camera;
+
+        private void Update()
+        {
+            if (_camera == null)
+            {
+                _camera = GameObject.FindWithTag("Camera");
+            }
+            Vector3 camForward = _camera.transform.forward;
+            Vector3 camUp = _camera.transform.up;
+
+            transform.LookAt(transform.position + camForward, camUp);
+        }
+    }
+}
